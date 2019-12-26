2efa5c3a1ecde49cb5e94dfe9e1dab0383965c1f
illusionist <illusionist.nemo@gmail.com>
2019-12-26 09:35:20 +0200

    add swagger

--- Sempl/Startup.cs
+++ Sempl/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Sempl
 {
@@ -12,6 +13,7 @@ namespace Sempl
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info {Title = "", Version = ""}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
