a073f82779cb6d5cd22fe9fa9318c902b59e3f45
illusionist <illusionist.nemo@gmail.com>
2020-03-26 12:54:07 +0200

    refactoring

--- Core/REST.Core/Extension/StringExtension.cs
+++ Core/REST.Core/Extension/StringExtension.cs
@@ -17,15 +17,5 @@ namespace REST.Core.Extension
             span[0] = char.ToUpper(span[0], CultureInfo.InvariantCulture);
             return new string(span);
         }
-        
-        public static bool IsNullOrEmpty(this string value)
-        {
-            return string.IsNullOrEmpty(value);
-        }
-
-        public static bool IsNullOrWhiteSpace(this string value)
-        {
-            return string.IsNullOrWhiteSpace(value);
-        }
     }
 }
