3a87c8fc6db386daa23fb3b3596678bf6b03278d
illusionist <illusionist.nemo@gmail.com>
2020-06-06 10:11:05 +0300

    add data ef postgres

---
+++ Core/Data/Radilovsoft.Rest.Data.Ef.Postgres/PostgresIndexProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Radilovsoft.Rest.Data.Ef.Contract;
+
+namespace Radilovsoft.Rest.Data.Ef.Postgres
+{
+    public class PostgresIndexProvider : IIndexProvider
+    {
+        public IndexBuilder HasMethod(IndexBuilder indexBuilder, string attributeMethod)
+        {
+            return indexBuilder.HasMethod(attributeMethod);
+        }
+    }
+}
